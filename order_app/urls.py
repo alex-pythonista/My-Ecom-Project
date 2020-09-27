9d5e4b1c4df3615b22d7edfd1aa51d6100596398
Shaon Indrajit Barua Alex <68987775+alex-pythonista@users.noreply.github.com>
2020-09-27 22:18:47 +0600

    commit

--- order_app/urls.py
+++ order_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from . import views
 
 app_name = 'order_app'
 
 urlpatterns = [
-    
+    path('add/<pk>/', views.add_to_cart, name='add'),
+
 ]
--- order_app/views.py
+++ order_app/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Cart, Order
+from shop_app.models import Product
 
 # Create your views here.
+
+@login_required
+def add_to_cart(request, pk):
+    item = get_object_or_404(Product, pk=pk)
+    order_item = Cart.objects.get_or_create(item=item, user=request.user, purchased=False)
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.order_items.filter(item=item).exists():
+            order_item[0].quantity += 1
+            order_item[0].save()
+            messages.info(request, 'This item quantity was updated.')
+            return redirect('shop_app:home')
+        else:
+            order.order_items.add(order_item[0])
+            messages.info(request, 'This item was added to your cart')
+            return redirect('shop_app:home')
+    else:
+        order = Order(user=request.user)
+        order.save()
+        order.order_items.add(order_item[0])
+        messages.info(request, 'This item was added to your cart')
+        return redirect('shop_app:home')
+
